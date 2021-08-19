6fb74633d48ef6a62a778eda24cc91070fa35bfc
Anna Ibraeva <a.ibraeva@ad.balance-pl.ru>
2021-08-19 19:59:15 +0300

    add state

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,54 +1,88 @@
+import React, { Component } from 'react';
 import './todo-list-item.css'
 
-// class TodoListItem extends React.Component {
+export default class TodoListItem extends Component {
 
-//   render() {
+    state = {
+      done: false,
+      important: false
+    }
 
-//     this.props;
+    onLabelClick = () => {
+      console.log(`Done: ${this.props.name}`)
+      this.setState((state) => {
+        return {
+          done: !state.done
+        }
+      })
+    }
 
-//     return (
-//       <span className="todo-list-item">
-//         <span
-//           className="todo-list-item-name">
-//           {name}
-//         </span>
-//         <button type="button"
-//                 className="btn done-btn icons">
-//           <i class="fas fa-exclamation-circle"></i>
-//         </button>
-  
-//         <button type="button"
-//                 className="btn delete-btn icons">
-//           <i className="fa fa-trash-o" />
-//         </button>
-//       </span>
-//     );
-//   }
-// }
+    onMarkImportant = () => {
+      this.setState((state) => {
+        return {
+          important: !state.important
+        }
+      })
+    }
 
-const TodoListItem = ({ name, important = false }) => {
-    
-  const style = {
-    color: important ? 'tomato' : 'black'
-  }
+  render() {
 
-  return (
-        <span style = {style} className="todo-list-item">
-          <span
-            className="todo-list-item-name">
-            {name}
-          </span>
-          <button type="button"
-                  className="btn done-btn icons">
-            <i className="fas fa-exclamation-circle"></i>
-          </button>
-    
-          <button type="button"
-                  className="btn delete-btn icons">
-            <i className="fa fa-trash-o" />
-          </button>
+    const { name } = this.props
+    const { done, important } = this.state
+
+    let classNames = 'todo-list-item'
+    if (done) {
+      classNames += ' done'
+    }
+
+    if (important) {
+      classNames += ' important'
+    }
+
+    return (
+      <span className={ classNames }>
+        <span
+          className="todo-list-item-name"
+          onClick = { this.onLabelClick }>
+          {name}
         </span>
-      );
+        <button type="button"
+                className="btn done-btn icons"
+                onClick = { this.onMarkImportant }>
+          <i class="fas fa-exclamation-circle"></i>
+        </button>
+  
+        <button type="button"
+                className="btn delete-btn icons">
+          <i className="fa fa-trash-o" />
+        </button>
+      </span>
+    );
+  }
 }
 
-export default TodoListItem;
+// const TodoListItemFunc = ({ name, important = false }) => {
+    
+//   const style = {
+//     color: important ? 'tomato' : 'black'
+//   }
+
+//   return (
+//         <span style = {style} className="todo-list-item">
+//           <span
+//             className="todo-list-item-name">
+//             {name}
+//           </span>
+//           <button type="button"
+//                   className="btn done-btn icons">
+//             <i className="fas fa-exclamation-circle"></i>
+//           </button>
+    
+//           <button type="button"
+//                   className="btn delete-btn icons">
+//             <i className="fa fa-trash-o" />
+//           </button>
+//         </span>
+//       );
+// }
+
